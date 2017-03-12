1c68a208cd2ee89b5ede69c4aa7cc2219c980e49
sandymechie <sandy.mechie@gmail.com>
2017-03-12 17:42:23 +0530

    changed package name Added brokenlinks method added drivers for Firefox and IE

---
+++ src/com/xceleratorninja/action/ExpectedActions.java
@@ -0,0 +1,14 @@
+package com.xceleratorninja.action;
+
+import java.util.List;
+
+public class ExpectedActions {
+	
+
+	public void checkForExpectation(String action,String expected,List<Object> actualresult){
+		
+		
+	
+	}
+
+}
